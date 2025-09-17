9259926d0735e4a577803ad9d26a8c363ecdc9e6
mini-bryanlin <bryanlin316@gmail.com>
2025-09-17 00:27:47 -0400

    better timing

--- src/app/page.js
+++ src/app/page.js
@@ -159,7 +159,7 @@ export default function Home() {
             text={["Bryan.", "a builder.", "a problem solver.", "a UWaterloo student."]}
             typingSpeed={75}
             pauseDuration={3500}
-            initialDelay={1500}
+            initialDelay={2000}
             showCursor={true}
             cursorCharacter="|"
             textColors={["#c4a3ec"]}
