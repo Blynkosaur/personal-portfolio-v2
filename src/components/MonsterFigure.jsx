5a78173b87d370075955ee2bf9a9ba0c1b1116f8
mini-bryanlin <bryanlin316@gmail.com>
2025-11-05 23:16:57 -0500

    monster figures

---
+++ src/components/MonsterFigure.jsx
@@ -0,0 +1,21 @@
+export default function Monster() {
+const figureOne = <pre>
+   \.-./
+ -(o o)-
+  | - |
+  | | |
+  '-' '-'
+  </pre>
+  const figureTwo = <pre>
+   .--.
+  (o_o )
+  //||\\
+ // || \\
+    // 
+  </pre>
+  return (
+    {figureOne}
+  )
+  
+}
+
