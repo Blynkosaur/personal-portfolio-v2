ac442a8780650fc5cd5c4ed1e159256e76dedff3
mini-bryanlin <bryanlin316@gmail.com>
2025-11-06 22:01:58 -0500

    more styling

--- src/components/MonsterFigure.jsx
+++ src/components/MonsterFigure.jsx
@@ -1,51 +1,84 @@
 import { motion } from "motion/react";
-function FigureOne(){ 
-  return(<motion.pre>{` 
-  \\\\.-.//
- -(o_o)-)
-  | _ | |
-  | | | |
-  '-' '-'
-  `}</motion.pre>)
+function FigureOne() {
+  return (
+    <motion.pre
+      className="cursor-target"
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.4,
+        scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+      }}
+    >{` \\\\.-.//
+-(o_o)-)
+ | _ | |
+ | | | |
+ '-' '-'`}</motion.pre>
+  );
 }
-function FigureTwo(){
 
-  const figureTwo = 
- `   .--.
+function FigureTwo() {
+  const figureTwo = `   .--.
   (o_o )
   //||\\\\
  // || \\\\
     //   `;
   return (
-  <motion.pre>{figureTwo}</motion.pre>
-  )
+    <motion.pre
+      className="cursor-target"
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.4,
+        scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+      }}
+    >
+      {figureTwo}
+    </motion.pre>
+  );
 }
-  function FigureThree (){ 
-  const figureThree = `
-  _  _
+function FigureThree() {
+  const figureThree = `  _   _
  / \\ / \\
 (o ) (o )
 /_/ o \\_\\
   //  \\\\
 `;
 
-return(<motion.pre>
- {figureThree}
-</motion.pre>)}
-
-  function  FigureFour (){ 
-  const figureFour = `
-   /@\`-'@\\
-  (  / \\  )
-   \\ | | /
-    \\|_|/
-        `;
-
-return(<motion.pre>{figureFour}</motion.pre>)
+  return (
+    <motion.pre
+      className="cursor-target"
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.4,
+        scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+      }}
+    >
+      {figureThree}
+    </motion.pre>
+  );
 }
-export {
-  FigureOne,
-  FigureTwo,
-  FigureThree,  
-  FigureFour
+
+function FigureFour() {
+  const figureFour = `  _   _
+ /@\`-'@\\
+(  / \\  )
+ \\ | | /
+  \\|_|/  `;
+
+  return (
+    <motion.pre
+      className="cursor-target"
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.4,
+        scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+      }}
+    >
+      {figureFour}
+    </motion.pre>
+  );
 }
+export { FigureOne, FigureTwo, FigureThree, FigureFour };
