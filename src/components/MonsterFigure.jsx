ab4967f8b9bae2ad766351c09b67ce3a35d7f3bb
mini-bryanlin <bryanlin316@gmail.com>
2025-11-09 16:41:53 -0500

    kill animation

--- src/components/MonsterFigure.jsx
+++ src/components/MonsterFigure.jsx
@@ -1,9 +1,41 @@
 import { motion } from "motion/react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 function FigureOne() {
   const mouth_ls = ["_", "*", "."];
   const [mouth, setMouth] = useState(mouth_ls[0]);
   const [mouth_index, setMouthIndex] = useState(0);
+  const [eyes, setEyes] = useState("o");
+  const [alive, setAlive] = useState(true);
+  const firstRenderAl = useRef(true);
+  const firstRenderVis = useRef(true);
+  const [visible, setVisible] = useState(true);
+  const handleClick = () => {
+    setEyes("x");
+    setTimeout(() => {
+      setAlive(False);
+    });
+  };
+  useEffect(() => {
+    if (firstRenderAl.current) {
+      firstRenderAl.current = false;
+      return;
+    }
+
+    setTimeout(() => {
+      setAlive(false);
+    }, 1500);
+  }, [eyes]);
+  useEffect(() => {
+    if (firstRenderVis.current) {
+      firstRenderVis.current = false;
+      return;
+    }
+
+    setTimeout(() => {
+      setVisible(false);
+    }, 500);
+  }, [alive]);
+
   useEffect(() => {
     const change = setInterval(() => {
       setMouthIndex((prevIndex) => {
@@ -13,27 +45,42 @@ function FigureOne() {
       });
     }, 1000);
   }, []);
-  return (
-    <motion.pre
-      className="cursor-target text-xs "
-      initial={{ opacity: 0, scale: 0 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{
-        duration: 0.4,
-        scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
-      }}
-    >{` \\\\.-.//
--(o${mouth}o)-)
+  const fig = ` \\\\.-.//
+-(${eyes}${mouth}${eyes})-)
  | _ | |
  | | | |
- '-' '-'`}</motion.pre>
+ '-' '-'`;
+  const flash = `
+  \\ * * /
+   - . . -
+   / * * \\
+`;
+  return (
+    visible && (
+      <motion.pre
+        onClick={handleClick}
+        className=" select-none cursor-target text-xs "
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 0.4,
+          scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+        }}
+      >
+        {alive ? fig : flash}
+      </motion.pre>
+    )
   );
 }
 
 function FigureTwo() {
-  const mouth_ls = ["_", "*", "."];
+  const mouth_ls = [".", "_", "*"];
   const [mouth, setMouth] = useState(mouth_ls[0]);
   const [mouth_index, setMouthIndex] = useState(0);
+  const [eyes, setEyes] = useState("o");
+  const handleClick = () => {
+    setEyes("x");
+  };
   useEffect(() => {
     const change = setInterval(() => {
       setMouthIndex((prevIndex) => {
@@ -45,13 +92,14 @@ function FigureTwo() {
   }, []);
 
   const figureTwo = `   .--.
-  (o${mouth}o )
+  (${eyes}${mouth}${eyes} )
   //||\\\\
  // || \\\\
     //   `;
   return (
     <motion.pre
-      className="cursor-target text-xs"
+      onClick={handleClick}
+      className="select-none cursor-target text-xs"
       initial={{ opacity: 0, scale: 0 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{
@@ -64,13 +112,18 @@ function FigureTwo() {
   );
 }
 function FigureThree() {
-  const mouth_ls = ["_", "*", "."];
+  const mouth_ls = [".", "*", "_", "o"];
   const [mouth, setMouth] = useState(mouth_ls[0]);
   const [mouth_index, setMouthIndex] = useState(0);
+  const [eyes, setEyes] = useState("o");
+  const handleClick = () => {
+    setEyes("x");
+  };
+
   useEffect(() => {
     const change = setInterval(() => {
       setMouthIndex((prevIndex) => {
-        const newIndex = (prevIndex + 1) % 3;
+        const newIndex = (prevIndex + 1) % 4;
         setMouth(mouth_ls[newIndex]);
         return newIndex;
       });
@@ -79,7 +132,7 @@ function FigureThree() {
 
   const figureThree = `  _   _
  / \\ / \\
-(o ) (o )
+(${eyes} ) (${eyes} )
 /_/ ${mouth} \\_\\
   //  \\\\
 `;
@@ -87,6 +140,7 @@ function FigureThree() {
   return (
     <motion.pre
       className="cursor-target text-xs"
+      onClick={handleClick}
       initial={{ opacity: 0, scale: 0 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{
@@ -100,13 +154,17 @@ function FigureThree() {
 }
 
 function FigureFour() {
-  const mouth_ls = ["_", "*", "."];
+  const mouth_ls = [".", "_", "*", "o"];
   const [mouth, setMouth] = useState(mouth_ls[0]);
   const [mouth_index, setMouthIndex] = useState(0);
+  const [eyes, setEyes] = useState("@");
+  const handleClick = () => {
+    setEyes("x");
+  };
   useEffect(() => {
     const change = setInterval(() => {
       setMouthIndex((prevIndex) => {
-        const newIndex = (prevIndex + 1) % 3;
+        const newIndex = (prevIndex + 1) % 4;
         setMouth(mouth_ls[newIndex]);
         return newIndex;
       });
@@ -114,14 +172,15 @@ function FigureFour() {
   }, []);
 
   const figureFour = `  _   _
- /@\`${mouth}'@\\
+ /${eyes}\`${mouth}'${eyes}\\
 (  / \\  )
  \\ | | /
   \\|_|/  `;
 
   return (
     <motion.pre
-      className="cursor-target text-xs"
+      className="cursor-target text-xs select-none "
+      onClick={handleClick}
       initial={{ opacity: 0, scale: 0 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{
@@ -149,7 +208,7 @@ function Door() {
 
   return (
     <motion.pre
-      className="cursor-target text-xs"
+      className="select-none  text-xs"
       initial={{ opacity: 0, scale: 0 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{
